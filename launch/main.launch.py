764388034640dddb5204c791b142320af264e3cc
Matthias Mayr <matthias.mayr@cs.lth.se>
2024-09-15 13:35:29 +0200

    New: Allows to pass problem_number to launch file & launch pyrobosim

--- launch/main.launch.py
+++ launch/main.launch.py
@@ -1,31 +1,61 @@
 import os
 from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, TextSubstitution
+from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
+    problem_number = LaunchConfiguration('problem_number')
+    start_pyrobosim = LaunchConfiguration('start_pyrobosim')
+
+    ld = LaunchDescription()
+    ld.add_action(
+        DeclareLaunchArgument(
+            "problem_number",
+            default_value="1",
+            description="The problem to start",
+        )
+    )
+    ld.add_action(
+        DeclareLaunchArgument(
+            "start_pyrobosim",
+            default_value="True",
+            description="Start pyrobosim world as well",
+        )
+    )
     skiros_config = {
         "libraries_list": "[skiros2_pyrobosim_lib, skiros2_std_skills]",
-        "skill_list": "[navigate, pick, place, open_location, close_location, open_door, close_door, charge,\
+        "skill_list": TextSubstitution(text=f"[navigate, pick, place, open_location, close_location, open_door, close_door, charge,\
                             navigate_execution, pick_execution, place_execution, open_execution, close_execution,\
-                            wm_set_relation, wm_move_object, wm_set_properties, bb_unset_param, success,\
-                            problem_1_solution, problem_2_solution]",
-        "init_scene": "p2_scene.turtle",
-        "verbose": "true",
-        "workspace_dir": get_package_share_directory('skiros2_pyrobosim_lib') + "/owl",
+                            wm_set_relation, wm_move_object, wm_set_properties, bb_unset_param,\
+                            problem_1_solution]"),
+        "init_scene": [TextSubstitution(text="p"), problem_number, TextSubstitution(text="_scene.turtle")],
+        "verbose": "false",
+        "workspace_dir": get_package_share_directory("skiros2_pyrobosim_lib") + "/owl",
         "robot_name": "robot",
-        "robot_ontology_prefix": "robi"
+        "robot_ontology_prefix": "robi",
     }
 
-    launch_include = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(
-                get_package_share_directory('skiros2'),
-                'skiros2.launch.py')),
-        launch_arguments=skiros_config.items(),
+    ld.add_action(
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(os.path.join(get_package_share_directory("skiros2"), "skiros2.launch.py")),
+            launch_arguments=skiros_config.items(),
+        )
     )
-     
-    return LaunchDescription([launch_include])
+
+    ld.add_action(
+        Node(
+            condition=IfCondition(start_pyrobosim),
+            package="delib_ws_worlds",
+            executable="run",
+            name="pyrobosim_world",
+            parameters=[{"problem_number": problem_number}],
+        ),
+    )
+
+    return ld
