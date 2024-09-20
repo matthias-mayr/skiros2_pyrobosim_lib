32a1cc55276475d8cff99e5b0ea301f8be10cf4b
Matthias Mayr <matthias.mayr@cs.lth.se>
2024-09-20 15:43:04 +0000

    New: Adds introduction to problem 1

---
+++ docs/1_Problem_1_Object_Fetch.md
@@ -0,0 +1,36 @@
+# Problem 1
+
+In the previous markdown file [0_Start.md](0_Start.md) we learned about the primitive skills and compound skills.
+
+The primitive skills interface directly with `pyrobosim` are in [src/skiros2_pyrobosim_lib/primitive_skills.py](../skiros2_pyrobosim_lib/primitive_skills.py).  
+The compound skills that use the and update the world model (WM) are in [src/skiros2_pyrobosim_lib/basic_compound_skills.py](../skiros2_pyrobosim_lib/basic_compound_skills.py).  
+
+Now we want to use them to solve problem 1. Navigate to [src/skiros2_pyrobosim_lib/problem_skills.py](../skiros2_pyrobosim_lib/problem_skills.py). You will find a skeleton for a compound skill to fill out:
+
+```python
+class problem_1(SkillBase):
+    def createDescription(self):
+        self.setDescription(description=Problem1(), label="Problem 1 - Fetch Item")
+
+    def expand(self, skill):
+        # We want to execute some skills in a sequence and remember which one we executed, so we use SerialStar. It will abort the skill if any of the child skills fail:
+        skill.setProcessor(SerialStar())
+        skill(
+            # As a first step, we want to navigate to the location where the object is currently located. In our skill, this is saved in the 'ObjectStartLocation' parameter.
+            # However, the 'Navigate' skill takes only a parameter 'TargetLocation', so we need to remap/rewire those parameters like this:
+            self.skill("Navigate", "", remap={"TargetLocation": "ObjectStartLocation"}),
+            # FIXME 1 - Add more skills to solve this task
+            # From here, feel free to add more skills. Check out 'basic_compound_skills.py' to see which ones are available:
+        )
+```
+
+You can search the codebase for `FIXME 1` for all spots where something for problem 1 needs to be done.
+
+## Using and Updating this Skill
+In order to use this skill, it needs to be in the list of skills to be loaded in `main.launch.py`. For the `problem_1` skill, this is already done.  
+From the GUI you can just select the skill and run it:
+
+
+
+After doing any changes in code, the skill needs to be loaded with the skill manager. You can either launch the whole launch file from scratch or especially in debug mode, just restart the skill manager node.
+
---
+++ skiros2_pyrobosim_lib/problem_skills.py
@@ -0,0 +1,52 @@
+from skiros2_skill.core.skill import SkillDescription, SkillBase, ParallelFs, ParallelFf, SerialStar, Selector
+from skiros2_common.core.params import ParamTypes
+from skiros2_common.core.world_element import Element
+
+#################################################################################
+# Descriptions
+#################################################################################
+
+class Problem1(SkillDescription):
+    def createDescription(self):
+        # =======Params=========
+        # A required parameter 'Object' that is a world-model element of the type 'Part':
+        self.addParam("Object", Element("skiros:Part"), ParamTypes.Required)
+        # Required parameter on where to bring the object of type world-model element 'Location':
+        self.addParam("ObjectTargetLocation", Element("skiros:Location"), ParamTypes.Required)
+        # An inferred parameter on the start (current) location of the object since we go there first and need to know where to go:
+        self.addParam("ObjectStartLocation", Element("skiros:Location"), ParamTypes.Inferred)
+
+        # =======PreConditions=========
+        # Precondition stating that 'ObjectStartLocation' must contain the object. Since we know the 'Object' from the required parameter, we can automatically fill out the 'ObjectStartLocation' with this rule:
+        self.addPreCondition(self.getRelationCond("ObjectContained", "skiros:contain", "ObjectStartLocation", "Object", True))
+        # =======PostConditions=========
+        # Finally a postcondition stating that after the Problem1 skill ran, the object must be contained by the 'ObjectTargetLocation'
+        self.addPostCondition(self.getRelationCond("ObjectContained", "skiros:contain", "ObjectTargetLocation", "Object", True))
+
+
+#################################################################################
+# Implementations
+#################################################################################
+
+class problem_1(SkillBase):
+    """
+    """
+    def createDescription(self):
+        self.setDescription(description=Problem1(), label="Problem 1 - Fetch Item")
+
+    def expand(self, skill):
+        # We want to execute some skills in a sequence and remember which one we executed, so we use SerialStar. It will abort the skill if any of the child skills fail:
+        skill.setProcessor(SerialStar())
+        skill(
+            # As a first step, we want to navigate to the location where the object is currently located. In our skill, this is saved in the 'ObjectStartLocation' parameter.
+            # However, the 'Navigate' skill takes only a parameter 'TargetLocation', so we need to remap/rewire those parameters like this:
+            self.skill("Navigate", "", remap={"TargetLocation": "ObjectStartLocation"}),
+            # FIXME 1 - Add more skills to solve this task
+            # From here, feel free to add more skills. Check out 'basic_compound_skills.py' to see which ones are available:
+
+
+
+
+            # This is for temporary cleanup - just ignore its existence:
+            self.skill("BbUnsetParam", "", remap={"Parameter": "StartLocation"}),
+        )
