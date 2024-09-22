88957e1c4af27ff2242d6a3d37d1a570f6898972
p-rosit <pontus.rosqvist@gmail.com>
2024-09-22 12:33:33 +0200

    search in wm with dijsktra

--- skiros2_pyrobosim_lib/reasoning_skills.py
+++ skiros2_pyrobosim_lib/reasoning_skills.py
@@ -1,3 +1,5 @@
+from enum import Enum, auto
+import skiros2_common.tools.logger as log
 from skiros2_skill.core.skill import SkillDescription
 from skiros2_common.core.primitive import PrimitiveBase
 from skiros2_common.core.params import ParamTypes
@@ -21,7 +23,7 @@ class SelectDoorsToTarget(SkillDescription):
         # =======Params=========
         self.addParam("Location", Element("skiros:Location"), ParamTypes.Required)
         self.addParam("TargetLocation", Element("skiros:Location"), ParamTypes.Required)
-        self.addParam("Door", Element("skiros:OpenableLocation"), ParamTypes.Optional)
+        self.addParam("IntermediateLocation", Element("skiros:Location"), ParamTypes.Optional)
 
 
 #################################################################################
@@ -66,6 +68,11 @@ class select_object_to_fetch(PrimitiveBase):
 
 
 class select_doors_to_target(PrimitiveBase):
+    class Edge(Enum):
+        door = auto()
+        room = auto()
+        sublocation = auto()
+
     def createDescription(self):
         self.setDescription(SelectDoorsToTarget(), "Select Closest Door Leading to Target")
 
@@ -74,58 +81,138 @@ class select_doors_to_target(PrimitiveBase):
 
     def find_path_between(self, start, target):
         goal_found = False
+
+        start_type = self.location_type(start)
+        if start_type is None:
+            log.warn(f"Start location is not a known type {start.label}: {start.type}")
+            return []
+
         visited_from = dict()
-        queue = [(None, None, start)]
+        queue = [(0, None, start_type, start)]
 
         while queue:
-            from_elm, door, elm = queue.pop(0)
+            res = min(queue, key=lambda x: x[0])
+            queue.remove(res)
+            distance, from_elm, elm_type, elm = res
 
             if elm.id in visited_from:
-                continue
+                prev, _ = visited_from[elm.id]
+                if prev < distance:
+                    continue
 
-            visited_from[elm.id] = (from_elm, door)
+            visited_from[elm.id] = (distance, from_elm)
 
             if elm.id == target.id:
                 goal_found = True
                 break
 
-            for door, to_elm in self.doors_from(elm):
-                queue.append((elm, door, to_elm))
+            for to_distance, to_type, to_elm in self.connections_from(elm_type, elm):
+                queue.append((distance + to_distance, elm, to_type, to_elm))
 
-        path = []
+        path = [target]
         if not goal_found:
             return path
 
-        elm = visited_from[target.id]
+        _, elm = visited_from[target.id]
         while elm is not None:
             path.append(elm)
-            elm = visited_from[elm.id]
+            _, elm = visited_from[elm.id]
 
+        log.warn('FOUND PATH: ' + str(list(reversed(path))))
         return list(reversed(path))
 
-    def doors_from(self, elm):
-        for relation in elm.getRelations(obj='-1', pred='skiros:adjacentLocation'):
-            door = self.wmi.get_element(relation['src'])
-            for door_relation in door.getRelations(subj='-1', pred='skiros:adjacentLocation'):
-                yield door, self.wmi.get_element(door_relation['dst'])
+    def connections_from(self, elm_type, elm):
+        if elm_type is self.Edge.door:
+            yield from self.connections_from_door(elm)
+        elif elm_type is self.Edge.room:
+            yield from self.connections_from_room(elm)
+        elif elm_type is self.Edge.sublocation:
+            yield from self.connections_from_sublocation(elm)
+        else:
+            raise NotImplementedError(elm_type)
+
+    def connections_from_door(self, elm):
+        for door_relation in elm.getRelations(subj='-1', pred='skiros:adjacentLocation'):
+            room = self.wmi.get_element(door_relation['dst'])
+            room_type = self.location_type(room)
+            if room_type is None:
+                continue
+            yield 2, room_type, room
+
+            for room_relation in room.getRelations(subj='-1', pred='skiros:contain'):
+                sublocation = self.wmi.get_element(room_relation['dst'])
+                sublocation_type = self.location_type(sublocation)
+                if sublocation_type is None:
+                    continue
+                yield 2, sublocation_type, sublocation
+
+            for room_relation in room.getRelations(obj='-1', pred='skiros:adjacentLocation'):
+                door = self.wmi.get_element(room_relation['src'])
+                door_type = self.location_type(door)
+                if door_type is None:
+                    continue
+                yield 1, door_type, door
+
+    def connections_from_room(self, elm):
+        for room_relation in elm.getRelations(obj='-1', pred='skiros:adjacentLocation'):
+            door = self.wmi.get_element(room_relation['src'])
+            door_type = self.location_type(door)
+            if door_type is None:
+                continue
+            yield 1, door_type, door
+
+        for room_relation in elm.getRelations(subj='-1', pred='skiros:contain'):
+            sublocation = self.wmi.get_element(room_relation['dst'])
+            sublocation_type = self.location_type(sublocation)
+            if sublocation_type is None:
+                continue
+            yield 2, sublocation_type, sublocation
+
+    def connections_from_sublocation(self, elm):
+        for sublocation_relation in elm.getRelations(obj='-1', pred='skiros:contain'):
+            room = self.wmi.get_element(sublocation_relation['src'])
+            room_type = self.location_type(room)
+            if room_type is None:
+                continue
+            yield 2, room_type, room
+
+            for room_relation in room.getRelations(obj='-1', pred='skiros:adjacentLocation'):
+                door = self.wmi.get_element(room_relation['src'])
+                door_type = self.location_type(door)
+                if door_type is None:
+                    continue
+                yield 1, door_type, door
+
+    def location_type(self, location):
+        cls = location.type
+
+        log.warn(f'Location type: {location.label}: {cls} <- {self.wmi.get_super_class(cls)}')
+        if cls == 'skiros:Door':
+            return self.Edge.door
+        elif cls == 'skiros:Room':
+            return self.Edge.room
+        elif cls == 'skiros:Location' or self.wmi.get_super_class(cls) in ['skiros:Location', 'skiros:OpenableLocation']:
+            return self.Edge.sublocation
+
+        return None
 
     def onStart(self):
-        self.current_door = None
+        self.current_location = None
         self.path = self.find_path_between(
-            self.params["Location"],
-            self.params["TargetLocation"],
+            self.params["Location"].value,
+            self.params["TargetLocation"].value,
         )
         return True
 
     def execute(self):
-        if self.current_door and self.current_door.id != self.params["Location"].id:
-            return self.step(f"Current location is not {self.current_door.label}")
+        if self.current_location and self.current_location.id != self.params["Location"].value.id:
+            return self.step(f"Current location is not {self.current_location.label}")
 
         if not self.path:
             return self.success("Path has been completed")
 
-        self.current_door = self.path[0]
+        self.current_location = self.path[0]
         self.path = self.path[1:]
-        self.params["Location"].value = self.current_door
-        return self.step(f"Door '{self.current_door.label}' selected")
+        self.params["IntermediateLocation"].value = self.current_location
+        return self.step(f"Door '{self.current_location.label}' selected")
 
