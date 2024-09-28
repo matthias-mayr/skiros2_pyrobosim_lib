6d38f853a759fb94def9f79fabad6633ca5146e8
p-rosit <pontus.rosqvist@gmail.com>
2024-09-28 13:59:03 +0200

    add `is_none`

--- skiros2_pyrobosim_lib/utils.py
+++ skiros2_pyrobosim_lib/utils.py
@@ -7,14 +7,28 @@ from skiros2_common.core.world_element import Element
 # Descriptions
 #################################################################################
 
+class IsNone(SkillDescription):
+    def createDescription(self):
+        self.addParam("Param", Element("skiros:Location"), ParamTypes.Optional)
+
 class CopyValue(SkillDescription):
     def createDescription(self):
         self.addParam("Input", Element("skiros:Location"), ParamTypes.Required)
         self.addParam("Output", Element("skiros:Location"), ParamTypes.Optional)
 
+
 #################################################################################
 # Implementations
 #################################################################################
+
+class is_none(PrimitiveBase):
+    def createDescription(self):
+        self.setDescription(IsNone(), "Is None")
+
+    def execute(self):
+        if self.params["Param"].value is None or self.params["Param"].value.id == '':
+            return self.success("Param was none")
+        return self.fail(f"Param is {self.params["Param"].value}", -1)
 
 class copy_value(PrimitiveBase):
     def createDescription(self):
